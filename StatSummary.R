24bb7936c7c24e67f85133094c0e77c91163615a
Raymond Tse <rtse999@gmail.com>
2018-01-04 11:13:42 +1100

    11:10 - Thursday 4 January 2018
    Changed class column to factor (TRUE/FALSE) to fix ordered lists
    returning 2 value in a class() function call
    
    Added number of levels and most common level columns

--- StatSummary.R
+++ StatSummary.R
@@ -1,17 +1,13 @@
 # ------------------------------------------------------------------------
-# Description: Statistical summary table
-# Link:
+# Description: Statistical summary table # Link:
 #
 # References:
-#   https://stackoverflow.com/questions/28144722/more-comprehensive-summary-function-in-r
+#  https://stackoverflow.com/questions/28144722/more-comprehensive-summary-function-in-r
 #
-# Location: /Users/raymondtse/Dropbox/Analysis/R Code/StatSummary.r
-# First created: 23:42 - Tuesday 2 January 2018
-# Last modified: 23:42 - Tuesday 2 January 2018
-# ------------------------------------------------------------------------
+# Location: /Users/raymondtse/Dropbox/Analysis/R Code/StatSummary.r # First created: 23:42 - Tuesday 2 January 2018 # Last modified: 23:42 - Tuesday 2 January 2018 # ------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------
-# System time 
+# System time
 # ------------------------------------------------------------------------
 format(Sys.time(), "%a %b %d %H:%M:%S %Y")
 
@@ -21,68 +17,79 @@ format(Sys.time(), "%a %b %d %H:%M:%S %Y")
 library(dplyr)
 
 # ------------------------------------------------------------------------
-# Functions to summarise a table of data
-# ------------------------------------------------------------------------
+# Functions to summarise a table of data # ------------------------------------------------------------------------
 summaryStats <- function(aVector) {
-  n <- NROW(aVector)
-  type <- typeof(aVector)
-  class <- class(aVector)
-  na <- sum(is.na(aVector))
-  min <- NA
-  median <- NA
-  max <- NA
-  sd <- NA
-  
-  if (is.numeric(aVector) == TRUE) {
-    min <- min(aVector)
-    median <- median(aVector)
-    max <- max(aVector)
-    sd <- round(sd(aVector),3)
-  } 
-  
-  summary <- list(
-    type = type, 
-    class = class,
-    n = n,
-    na = na,
-    min = min,
-    median = median,
-    max = max,
-    sd = sd
-  )
-  return(summary)
+ n <- NROW(aVector)
+ type <- typeof(aVector)
+ factor <- is.factor(aVector)
+ na <- sum(is.na(aVector))
+ min <- NA
+ median <- NA
+ max <- NA
+ sd <- NA
+ levels <- NA
+ most <- NA
+
+ if (is.numeric(aVector) == TRUE) {
+   min <- min(aVector)
+   median <- median(aVector)
+   max <- max(aVector)
+   sd <- round(sd(aVector),3)
+ } else if (is.factor(aVector) == TRUE) {
+   levels <- nlevels(aVector)
+   most <- names(which.max(table(aVector)))
+ }
+
+ summary <- list(
+   type = type, 
+   factor = factor,
+   n = n,
+   na = na,
+   min = min,
+   median = median,
+   max = max,
+   sd = sd,
+   levels = levels,
+   most = most
+ )
+ return(summary)
 }
 
 summaryStatsTbl <- function(aTable) {
-  tbl <- data.frame()
-  for (i in seq_along(aTable)) {
-    cols <- data.frame(summaryStats(aTable[[i]]))
-    newRow <- data.frame("name" = colnames(aTable[i]))
-    newRow <- bind_cols(newRow, cols)
-  
-    tbl <- bind_rows(tbl, newRow)
-  }
-  return(tbl)
+ tbl <- data.frame()
+ for (i in seq_along(aTable)) {
+   cols <- data.frame(summaryStats(aTable[[i]]))
+   newRow <- data.frame("name" = colnames(aTable[i]))
+   newRow <- bind_cols(newRow, cols)
+
+   tbl <- bind_rows(tbl, newRow)
+ }
+ return(tbl)
 }
 # work out how to suppress warnings
-# factors - number of levels, most common level
-# characters - min length, max length, median length
+# characters - min length, max length, median length 
 # logical - true, false
-
 
 
 # ------------------------------------------------------------------------
 # Test scripts
 # ------------------------------------------------------------------------
+testRow1 <- NULL
 testRow1 <- summaryStats(mtcars$mpg) # numeric field
 testRow1
 
+testRow2 <- NULL
 testRow2 <- summaryStats(iris$Species) # factor field
 testRow2
 
+testTbl1 <- NULL
 testTbl1 <- summaryStatsTbl(mtcars) # dataframe of all numeric columns
 testTbl1
 
+testTbl2 <- NULL
 testTbl2 <- summaryStatsTbl(iris) # dataframe of numeric and factor columns
 testTbl2
 
+testTbl3 <- NULL
+testTbl3 <- summaryStatsTbl(esoph) # can the function handle ordered factors ?
+testTbl3
